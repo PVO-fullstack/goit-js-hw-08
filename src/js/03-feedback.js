a8fa46f03251df3d23424b302a2cb2753636208a
Viacheslav Plevako <113464187+PVO-fullstack@users.noreply.github.com>
2023-01-13 17:16:20 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector('.feedback-form');
 
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, message }
+      } = event.currentTarget;
+      if (email.value === '' || message.value === '') {
+        alert('Bсі поля повинні бути заповнені');
+      } else {
+      const data = {
+        email: email.value,
+        message: message.value,
+      }
+      console.log(data);
+      localStorage.setItem('feedback-form-state', JSON.stringify(data));
+      event.currentTarget.reset();    
+      }
+}
+
+const savedData = localStorage.getItem('feedback-form-state');
+const parsedData = JSON.parse(savedData);
+console.log(parsedData);
