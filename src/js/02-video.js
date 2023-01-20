c2145ceee80622a4105ab6cadc10a2db31e56d94
Viacheslav Plevako <cheryec@ukr.net>
2023-01-20 10:36:53 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,24 +4,22 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const throttle = require('lodash.throttle');
 
-const timeForStart = localStorage.getItem('videoplayer-current-time');
+let timeForStart = localStorage.getItem('videoplayer-current-time');
+
+console.log(timeForStart);
 
 player.on('play', onPlay);
-player.on('pause', onPause);
 player.on('timeupdate', throttle(pastVideo, 1000));
 
-function onPause() {
-  player.off('play');
-  // console.log('зняли слухача play');
-};
-
 function onPlay() {
+  timeForStart = localStorage.getItem('videoplayer-current-time');
   player.setCurrentTime(timeForStart);
 };
 
 function pastVideo(currentTime) {
   const viewedTime = currentTime.seconds;
   localStorage.setItem('videoplayer-current-time', viewedTime);
+  console.log(viewedTime);
 };
 
 
